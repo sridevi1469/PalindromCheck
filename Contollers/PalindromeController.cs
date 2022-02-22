93589d4f20396f576f1387ae3655ff6562ed5f80
sridevi1469 <63106708+sridevi1469@users.noreply.github.com>
2022-02-22 11:32:06 +1100

    Add files via upload

---
+++ Contollers/PalindromeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PaliAPI.Model;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PaliAPI.Contollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PalindromeController : ControllerBase
+    {
+        // GET: api/<PalindromeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PalindromeController>/5
+        [HttpGet("{PaliString}")]
+        public ActionResult<string> GetPalindromeString(string PaliString)
+        {
+            PaliCheck ps = new PaliCheck();
+
+            return(ps.longestPali(PaliString));
+
+
+          
+        }
+
+       
+      
+    }
+}
---
+++ Model/PaliCheck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaliAPI.Model
+{
+    public class PaliCheck
+    {
+       
+      
+         
+        public  string longestPali(string str)
+        {
+
+            if (str == null || str.Length < 1) return "";
+
+            int start = 0; int end = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                int len1 = expandFromMiddle(str, i, i);
+                int len2 = expandFromMiddle(str, i, i + 1);
+                int len = Math.Max(len1, len2);
+                if (len > end - start)
+                {
+                    start = i - ((len - 1) / 2);
+                    end = i + (len / 2);
+                }
+            }
+            return str.Substring(start, end - start + 1);
+
+
+        }
+
+        public  int expandFromMiddle(string s, int leftVal, int rightVal)
+        {
+            char[] charA = s.ToCharArray();
+            if (s == null || leftVal > rightVal) return 0;
+
+            while (leftVal >= 0 && rightVal < s.Length && charA[leftVal] == charA[rightVal])
+            {
+                leftVal--;
+                rightVal++;
+            }
+
+            return rightVal - leftVal - 1;
+        }
+
+
+    }
+}
+
